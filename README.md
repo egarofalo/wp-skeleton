866f1f2f8811529741d89fe8b6fc56bef24323cb
egarofalo <egarofalo83@gmail.com>
2017-07-31 11:24:41 -0300

    Cambios ------- - Modificación del archivo README.md - Se agregaron los siguientes archivos: 	- config/**/*

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ Wordpress Skeleton, integrado con Composer y Yaml.
 
 Wordpress Skeleton es un esqueleto de Wordpress que sigue las mejores prácticas de cómo se debe estructurar los archivos de un framework MVC y administrar las dependencias del mismo. Los archivos del core de Wordpress están ubicados en una subcarpeta, llamada wp. Esto ayuda al desarrollador a diferenciar los archivos propios del proyecto de los archivos del core de Wordpress. Por otro lado se utiliza Composer para instalar las dependencias, ya sean librerías de PHP, temas y plugins de Wordpress. Finalmente wp-skeleton dispone de una seguridad adicional a través de los archivos .htaccess.
 
+# Instalación
+
+Descargue el proyecto desde github o a través del comando git clone, y luego ejecute el comando composer install dentro de la raíz del proyecto (ubicación del archivo composer.json).
+
 # Actualizando las dependencias
 
 Actualiza las librerías PHP, plugins, temas y el core de Wordpress (dependencias) con Composer.
---
+++ config/ComposerSetup.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace CoDevelopers\WpSkeleton\Config;
+
+use Composer\Script\Event;
+use Composer\Installer\PackageEvent;
+
+class ComposerSetup {
+
+    private static $output = 'Configuring Wordpress Skeleton...';
+
+    public static function postUpdate(Event $event) {
+        self::setupProject();
+    }
+
+    public static function postInstall(Event $event) {
+        self::setupProject();
+    }
+
+    public static function postCreateProject(PackageEvent $event) {
+        self::setupProject();
+    }
+
+    private static function setupProject() {
+        // output the message
+        echo self::$output;
+        // remove wp/wp-content folder
+        if (file_exists(dirname(__FILE__) . '/../wp/wp-content')) {
+            self::rrmdir(dirname(__FILE__) . '/../wp/wp-content');
+        }
+        // protect vendor folder with an .htaccess file
+        copy(dirname(__FILE__) . '/.htaccess', dirname(__FILE__) . '/../vendor/.htaccess');
+    }
+
+    private static function rrmdir($dir) {
+        if (is_dir($dir)) {
+            $files = scandir($dir);
+            foreach ($files as $file) {
+                if ($file != "." && $file != "..") {
+                    self::rrmdir("{$dir}/{$file}");
+                }
+            }
+            rmdir($dir);
+        } elseif (file_exists($dir)) {
+            unlink($dir);
+        }
+    }
+
+}
